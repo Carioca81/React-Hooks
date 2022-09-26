a6341ff81b48d7b5abaacd1eb765f4952a817a83
Marcelo de Souza Pereira <marcelobct@gmail.com>
2022-09-26 01:44:10 -0300

    Completado o desafio de react hooks

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+
+const calcFatorial = num => {
+    const n = parseInt(num)
+    if(n < 0) return -1
+    if(n === 0) return 1
+    return calcFatorial(n-1) * n
+}
+
+const parOuImpar = (num) => {
+    const n = parseInt(num)
+    return ((n % 2) === 0) ? 'PAR':'ÍMPAR'
+}
 
 const UseEffect = (props) => {
+    // Ex. #01
+    const [numero, setNumero] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+    
+    useEffect(function(){
+        setFatorial(calcFatorial(numero))
+    },[numero])
+    
+    useEffect(()=>{
+        if(fatorial > 1000000){
+            document.title = 'Eita!!!'
+        }
+    },[fatorial])
+    
+    // Ex. #02
+    const [parImpar, setParImpar] = useState('?')
+    useEffect(()=>{
+        setParImpar(parOuImpar(numero))
+    },[numero])
+
+    const divStyle = {
+        marginTop: '1em',
+    }
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercício #01"/>
+            <div className='center'>
+                <div>
+                    <span className='text'>Fatorial:</span>
+                    <span className='text red'>{fatorial < 0 ?'Não existe': fatorial }</span>
+                </div>
+                <input type="number" className="input" value={numero} onChange={e => setNumero(e.target.value)}/>
+            </div>
+            <SectionTitle title="Exercício #02"/>
+            <div className='center'>
+                <div style={divStyle}>
+                    <span className='text'>O valor {numero} é</span>
+                    <span className='text red'> {parImpar}</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+
+function mesclar(s1, s2){
+    let textoNaoFormatado = s1+s2
+    let arr1 = s1.split('')
+    let arr2 = s2.split('')
+    let tam = arr1.length + arr2.length
+    // console.log(tam)
+    let text = []
+    let x = 0
+    let y = 0
+
+    for(let i = 0; i < tam; i++){
+        if(i%2 === 0){
+            if(x < arr1.length) {
+                text.push(arr1[x])
+                x++
+            }
+        }else{
+            if(y < arr2.length){
+                text.push(arr2[y])
+                y++
+            }
+        }
+    }
+    return text
+}
+
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState('')
+    const [value2, setValue2] = useState('')
+    const count = useRef(0)
+
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
+    useEffect(()=>{
+        count.current = count.current + 1
+        myInput2.current.focus()
+    }, [value1])
+
+    useEffect(()=>{
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercício #01"/>
+            <div className='center'>
+                <div>
+                    <span className='text'>Valor: </span>
+                    <span className='text'>{mesclar(value1, value2)} [</span>
+                    <span className='text red'>{count.current}</span>
+                    <span className='text'>]</span>
+                </div>
+                <input type="text" className='input' ref={myInput1} value={value1} onChange={e => setValue1(e.target.value)}/>
+            </div>
+            <SectionTitle title="Exercício #02"/>
+            <div className='center'>
+                <input type='text' className='input' ref={myInput2} value={value2} onChange={e => setValue2(e.target.value)}/>
+            </div>
         </div>
     )
 }
